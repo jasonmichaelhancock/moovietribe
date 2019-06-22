21995964b6c3dcf9f74441f1c90d3f07887bd385
Jason Michael Hancock <jasonmichaelhancock@gmail.com>
2019-06-22 10:57:58 +0000

    moovietribe upload

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,8 +5,8 @@ from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
-from app.main.forms import EditProfileForm, PostForm, SearchForm, MessageForm
-from app.models import User, Post, Message, Notification
+from app.main.forms import EditProfileForm, EditProfileForm2, EditProfileForm3, PostForm, SearchForm, MessageForm
+from app.models import User, Post, Message, Notification, Moovies
 from app.translate import translate
 from app.main import bp
 
@@ -98,6 +98,235 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title=_('Edit Profile'),
+                           form=form)
+
+
+@bp.route('/edit_profile2', methods=['GET', 'POST'])
+@login_required
+def edit_profile2():
+    form = EditProfileForm2(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.film1 = form.film1.data
+        current_user.film2 = form.film2.data
+        current_user.film3 = form.film3.data
+        current_user.film4 = form.film4.data
+        current_user.film5 = form.film5.data
+        current_user.film6 = form.film6.data
+        current_user.film7 = form.film7.data
+        current_user.film8 = form.film8.data
+        current_user.film9 = form.film9.data
+        current_user.film10 = form.film10.data
+        current_user.film11 = form.film11.data
+        current_user.film12 = form.film12.data
+        current_user.film13 = form.film13.data
+        current_user.film14 = form.film14.data
+        current_user.film15 = form.film15.data
+        current_user.film16 = form.film16.data
+        current_user.film17 = form.film17.data
+        current_user.film18 = form.film18.data
+        current_user.film19 = form.film19.data
+        current_user.film20 = form.film20.data
+        current_user.film21 = form.film21.data
+        current_user.film22 = form.film22.data
+        current_user.film23 = form.film23.data
+        current_user.film24 = form.film24.data
+        current_user.film25 = form.film25.data
+        current_user.film26 = form.film26.data
+        current_user.film27 = form.film27.data
+        current_user.film28 = form.film28.data
+        current_user.film29 = form.film29.data
+        current_user.film30 = form.film30.data
+        current_user.film31 = form.film31.data
+        current_user.film32 = form.film32.data
+        current_user.film33 = form.film33.data
+        current_user.film34 = form.film34.data
+        current_user.film35 = form.film35.data
+        current_user.film36 = form.film36.data
+        current_user.film37 = form.film37.data
+        current_user.film38 = form.film38.data
+        current_user.film39 = form.film39.data
+        current_user.film40 = form.film40.data
+        current_user.film41 = form.film41.data
+        current_user.film42 = form.film42.data
+        current_user.film43 = form.film43.data
+        current_user.film44 = form.film44.data
+        current_user.film45 = form.film45.data
+        current_user.film46 = form.film46.data
+        current_user.film47 = form.film47.data
+        current_user.film48 = form.film48.data
+        current_user.film49 = form.film49.data
+        current_user.film50 = form.film50.data
+        current_user.film51 = form.film51.data
+        current_user.film52 = form.film52.data
+        current_user.film53 = form.film53.data
+        current_user.film54 = form.film54.data
+        current_user.film55 = form.film55.data
+        current_user.film56 = form.film56.data
+        current_user.film57 = form.film57.data
+        current_user.film58 = form.film58.data
+        current_user.film59 = form.film59.data
+        current_user.film60 = form.film60.data
+        current_user.film61 = form.film61.data
+        current_user.film62 = form.film62.data
+        current_user.film63 = form.film63.data
+        current_user.film64 = form.film64.data
+        current_user.film65 = form.film65.data
+        current_user.film66 = form.film66.data
+        current_user.film67 = form.film67.data
+        current_user.film68 = form.film68.data
+        current_user.film69 = form.film69.data
+        current_user.film70 = form.film70.data
+        current_user.film71 = form.film71.data
+        current_user.film72 = form.film72.data
+        current_user.film73 = form.film73.data
+        current_user.film74 = form.film74.data
+        current_user.film75 = form.film75.data
+        current_user.film76 = form.film76.data
+        current_user.film77 = form.film77.data
+        current_user.film78 = form.film78.data
+        current_user.film79 = form.film79.data
+        current_user.film80 = form.film80.data
+        current_user.film81 = form.film81.data
+        current_user.film82 = form.film82.data
+        current_user.film83 = form.film83.data
+        current_user.film84 = form.film84.data
+        current_user.film85 = form.film85.data
+        current_user.film86 = form.film86.data
+        current_user.film87 = form.film87.data
+        current_user.film88 = form.film88.data
+        current_user.film89 = form.film89.data
+        current_user.film90 = form.film90.data
+        current_user.film91 = form.film91.data
+        current_user.film92 = form.film92.data
+        current_user.film93 = form.film93.data
+        current_user.film94 = form.film94.data
+        current_user.film95 = form.film95.data
+        current_user.film96 = form.film96.data
+        current_user.film97 = form.film97.data
+        current_user.film98 = form.film98.data
+        current_user.film99 = form.film99.data
+        current_user.film100 = form.film100.data
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.film1.data = current_user.film1
+        form.film2.data = current_user.film2
+        form.film3.data = current_user.film3
+        form.film4.data = current_user.film4
+        form.film5.data = current_user.film5
+        form.film6.data = current_user.film6
+        form.film7.data = current_user.film7
+        form.film8.data = current_user.film8
+        form.film9.data = current_user.film9
+        form.film10.data = current_user.film10
+        form.film11.data = current_user.film11
+        form.film12.data = current_user.film12
+        form.film13.data = current_user.film13
+        form.film14.data = current_user.film14
+        form.film15.data = current_user.film15
+        form.film16.data = current_user.film16
+        form.film17.data = current_user.film17
+        form.film18.data = current_user.film18
+        form.film19.data = current_user.film19
+        form.film20.data = current_user.film20
+        form.film21.data = current_user.film21
+        form.film22.data = current_user.film22
+        form.film23.data = current_user.film23
+        form.film24.data = current_user.film24
+        form.film25.data = current_user.film25
+        form.film26.data = current_user.film26
+        form.film27.data = current_user.film27
+        form.film28.data = current_user.film28
+        form.film29.data = current_user.film29
+        form.film30.data = current_user.film30
+        form.film31.data = current_user.film31
+        form.film32.data = current_user.film32
+        form.film33.data = current_user.film33
+        form.film34.data = current_user.film34
+        form.film35.data = current_user.film35
+        form.film36.data = current_user.film36
+        form.film37.data = current_user.film37
+        form.film38.data = current_user.film38
+        form.film39.data = current_user.film39
+        form.film40.data = current_user.film40
+        form.film41.data = current_user.film41
+        form.film42.data = current_user.film42
+        form.film43.data = current_user.film43
+        form.film44.data = current_user.film44
+        form.film45.data = current_user.film45
+        form.film46.data = current_user.film46
+        form.film47.data = current_user.film47
+        form.film48.data = current_user.film48
+        form.film49.data = current_user.film49
+        form.film50.data = current_user.film50
+        form.film51.data = current_user.film51
+        form.film52.data = current_user.film52
+        form.film53.data = current_user.film53
+        form.film54.data = current_user.film54
+        form.film55.data = current_user.film55
+        form.film56.data = current_user.film56
+        form.film57.data = current_user.film57
+        form.film58.data = current_user.film58
+        form.film59.data = current_user.film59
+        form.film60.data = current_user.film60
+        form.film61.data = current_user.film61
+        form.film62.data = current_user.film62
+        form.film63.data = current_user.film63
+        form.film64.data = current_user.film64
+        form.film65.data = current_user.film65
+        form.film66.data = current_user.film66
+        form.film67.data = current_user.film67
+        form.film68.data = current_user.film68
+        form.film69.data = current_user.film69
+        form.film70.data = current_user.film70
+        form.film71.data = current_user.film71
+        form.film72.data = current_user.film72
+        form.film73.data = current_user.film73
+        form.film74.data = current_user.film74
+        form.film75.data = current_user.film75
+        form.film76.data = current_user.film76
+        form.film77.data = current_user.film77
+        form.film78.data = current_user.film78
+        form.film79.data = current_user.film79
+        form.film80.data = current_user.film80
+        form.film81.data = current_user.film81
+        form.film82.data = current_user.film82
+        form.film83.data = current_user.film83
+        form.film84.data = current_user.film84
+        form.film85.data = current_user.film85
+        form.film86.data = current_user.film86
+        form.film87.data = current_user.film87
+        form.film88.data = current_user.film88
+        form.film89.data = current_user.film89
+        form.film90.data = current_user.film90
+        form.film91.data = current_user.film91
+        form.film92.data = current_user.film92
+        form.film93.data = current_user.film93
+        form.film94.data = current_user.film94
+        form.film95.data = current_user.film95
+        form.film96.data = current_user.film96
+        form.film97.data = current_user.film97
+        form.film98.data = current_user.film98
+        form.film99.data = current_user.film99
+        form.film100.data = current_user.film100
+    return render_template('edit_profile2.html', title=_('Edit Movies'),
+                           form=form)
+
+
+@bp.route('/edit_profile3', methods=['GET', 'POST'])
+@login_required
+def edit_profile3():
+    form = EditProfileForm3()
+    if form.validate_on_submit():
+        moovies = Moovies(imdb=form.imdb.data, poster=form.poster.data)
+        db.session.add(moovies)
+        db.session.commit()
+        flash('Moovie added!')
+        return redirect(url_for('main.edit_profile3'))
+    return render_template('edit_profile3.html', title=_('Edit Profile'),
                            form=form)
 
 
--- app/models.py
+++ app/models.py
@@ -95,6 +95,106 @@ class User(UserMixin, PaginatedAPIMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
+    film1 = db.Column(db.Integer)
+    film2 = db.Column(db.Integer)
+    film3 = db.Column(db.Integer)
+    film4 = db.Column(db.Integer)
+    film5 = db.Column(db.Integer)
+    film6 = db.Column(db.Integer)
+    film7 = db.Column(db.Integer)
+    film8 = db.Column(db.Integer)
+    film9 = db.Column(db.Integer)
+    film10 = db.Column(db.Integer)
+    film11 = db.Column(db.Integer)
+    film12 = db.Column(db.Integer)
+    film13 = db.Column(db.Integer)
+    film14 = db.Column(db.Integer)
+    film15 = db.Column(db.Integer)
+    film16 = db.Column(db.Integer)
+    film17 = db.Column(db.Integer)
+    film18 = db.Column(db.Integer)
+    film19 = db.Column(db.Integer)
+    film20 = db.Column(db.Integer)
+    film21 = db.Column(db.Integer)
+    film22 = db.Column(db.Integer)
+    film23 = db.Column(db.Integer)
+    film24 = db.Column(db.Integer)
+    film25 = db.Column(db.Integer)
+    film26 = db.Column(db.Integer)
+    film27 = db.Column(db.Integer)
+    film28 = db.Column(db.Integer)
+    film29 = db.Column(db.Integer)
+    film30 = db.Column(db.Integer)
+    film31 = db.Column(db.Integer)
+    film32 = db.Column(db.Integer)
+    film33 = db.Column(db.Integer)
+    film34 = db.Column(db.Integer)
+    film35 = db.Column(db.Integer)
+    film36 = db.Column(db.Integer)
+    film37 = db.Column(db.Integer)
+    film38 = db.Column(db.Integer)
+    film39 = db.Column(db.Integer)
+    film40 = db.Column(db.Integer)
+    film41 = db.Column(db.Integer)
+    film42 = db.Column(db.Integer)
+    film43 = db.Column(db.Integer)
+    film44 = db.Column(db.Integer)
+    film45 = db.Column(db.Integer)
+    film46 = db.Column(db.Integer)
+    film47 = db.Column(db.Integer)
+    film48 = db.Column(db.Integer)
+    film49 = db.Column(db.Integer)
+    film50 = db.Column(db.Integer)
+    film51 = db.Column(db.Integer)
+    film52 = db.Column(db.Integer)
+    film53 = db.Column(db.Integer)
+    film54 = db.Column(db.Integer)
+    film55 = db.Column(db.Integer)
+    film56 = db.Column(db.Integer)
+    film57 = db.Column(db.Integer)
+    film58 = db.Column(db.Integer)
+    film59 = db.Column(db.Integer)
+    film60 = db.Column(db.Integer)
+    film61 = db.Column(db.Integer)
+    film62 = db.Column(db.Integer)
+    film63 = db.Column(db.Integer)
+    film64 = db.Column(db.Integer)
+    film65 = db.Column(db.Integer)
+    film66 = db.Column(db.Integer)
+    film67 = db.Column(db.Integer)
+    film68 = db.Column(db.Integer)
+    film69 = db.Column(db.Integer)
+    film70 = db.Column(db.Integer)
+    film71 = db.Column(db.Integer)
+    film72 = db.Column(db.Integer)
+    film73 = db.Column(db.Integer)
+    film74 = db.Column(db.Integer)
+    film75 = db.Column(db.Integer)
+    film76 = db.Column(db.Integer)
+    film77 = db.Column(db.Integer)
+    film78 = db.Column(db.Integer)
+    film79 = db.Column(db.Integer)
+    film80 = db.Column(db.Integer)
+    film81 = db.Column(db.Integer)
+    film82 = db.Column(db.Integer)
+    film83 = db.Column(db.Integer)
+    film84 = db.Column(db.Integer)
+    film85 = db.Column(db.Integer)
+    film86 = db.Column(db.Integer)
+    film87 = db.Column(db.Integer)
+    film88 = db.Column(db.Integer)
+    film89 = db.Column(db.Integer)
+    film90 = db.Column(db.Integer)
+    film91 = db.Column(db.Integer)
+    film92 = db.Column(db.Integer)
+    film93 = db.Column(db.Integer)
+    film94 = db.Column(db.Integer)
+    film95 = db.Column(db.Integer)
+    film96 = db.Column(db.Integer)
+    film97 = db.Column(db.Integer)
+    film98 = db.Column(db.Integer)
+    film99 = db.Column(db.Integer)
+    film100 = db.Column(db.Integer)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     token = db.Column(db.String(32), index=True, unique=True)
     token_expiration = db.Column(db.DateTime)
@@ -194,6 +294,106 @@ class User(UserMixin, PaginatedAPIMixin, db.Model):
             'username': self.username,
             'last_seen': self.last_seen.isoformat() + 'Z',
             'about_me': self.about_me,
+            'film1': self.film1,
+            'film2': self.film2,
+            'film3': self.film3,
+            'film4': self.film4,
+            'film5': self.film5,
+            'film6': self.film6,
+            'film7': self.film7,
+            'film8': self.film8,
+            'film9': self.film9,
+            'film10': self.film10,
+            'film11': self.film11,
+            'film12': self.film12,
+            'film13': self.film13,
+            'film14': self.film14,
+            'film15': self.film15,
+            'film16': self.film16,
+            'film17': self.film17,
+            'film18': self.film18,
+            'film19': self.film19,
+            'film20': self.film20,
+            'film21': self.film21,
+            'film22': self.film22,
+            'film23': self.film23,
+            'film24': self.film24,
+            'film25': self.film25,
+            'film26': self.film26,
+            'film27': self.film27,
+            'film28': self.film28,
+            'film29': self.film29,
+            'film30': self.film30,
+            'film31': self.film31,
+            'film32': self.film32,
+            'film33': self.film33,
+            'film34': self.film34,
+            'film35': self.film35,
+            'film36': self.film36,
+            'film37': self.film37,
+            'film38': self.film38,
+            'film39': self.film39,
+            'film40': self.film40,
+            'film41': self.film41,
+            'film42': self.film42,
+            'film43': self.film43,
+            'film44': self.film44,
+            'film45': self.film45,
+            'film46': self.film46,
+            'film47': self.film47,
+            'film48': self.film48,
+            'film49': self.film49,
+            'film50': self.film50,
+            'film51': self.film51,
+            'film52': self.film52,
+            'film53': self.film53,
+            'film54': self.film54,
+            'film55': self.film55,
+            'film56': self.film56,
+            'film57': self.film57,
+            'film58': self.film58,
+            'film59': self.film59,
+            'film60': self.film60,
+            'film61': self.film61,
+            'film62': self.film62,
+            'film63': self.film63,
+            'film64': self.film64,
+            'film65': self.film65,
+            'film66': self.film66,
+            'film67': self.film67,
+            'film68': self.film68,
+            'film69': self.film69,
+            'film70': self.film70,
+            'film71': self.film71,
+            'film72': self.film72,
+            'film73': self.film73,
+            'film74': self.film74,
+            'film75': self.film75,
+            'film76': self.film76,
+            'film77': self.film77,
+            'film78': self.film78,
+            'film79': self.film79,
+            'film80': self.film80,
+            'film81': self.film81,
+            'film82': self.film82,
+            'film83': self.film83,
+            'film84': self.film84,
+            'film85': self.film85,
+            'film86': self.film86,
+            'film87': self.film87,
+            'film88': self.film88,
+            'film89': self.film89,
+            'film90': self.film90,
+            'film91': self.film91,
+            'film92': self.film92,
+            'film93': self.film93,
+            'film94': self.film94,
+            'film95': self.film95,
+            'film96': self.film96,
+            'film97': self.film97,
+            'film98': self.film98,
+            'film99': self.film99,
+            'film100': self.film100,
             'post_count': self.posts.count(),
             'follower_count': self.followers.count(),
             'followed_count': self.followed.count(),
@@ -209,7 +409,15 @@ class User(UserMixin, PaginatedAPIMixin, db.Model):
         return data
 
     def from_dict(self, data, new_user=False):
-        for field in ['username', 'email', 'about_me']:
+        for field in ['username', 'email', 'about_me', 'film1', 'film2', 'film3', 'film4', 'film5', 'film6', 'film7', 'film8', 'film9', 'film10', 'film11', 'film12', 'film13', 'film14', 'film15', 'film16', 'film17', 'film18', 'film19', 'film20', 'film21', 'film22', 'film23', 'film24', 'film25', 'film26', 'f\
+        ilm27', 'film28', 'film29', 'film30', 'film31', 'film32', 'film33', 'film34', 'film35', 'film36', 'f\
+        ilm37', 'film38', 'film39', 'film40', 'film41', 'film32', 'film43', 'film44', 'film45', 'film46', 'f\
+        ilm47', 'film48', 'film49', 'film50', 'film51', 'film32', 'film53', 'film54', 'film55', 'film56', 'f\
+        ilm57', 'film58', 'film59', 'film60', 'film61', 'film32', 'film63', 'film64', 'film65', 'film66', 'f\
+        ilm67', 'film68', 'film69', 'film70', 'film71', 'film32', 'film73', 'film74', 'film75', 'film76', 'f\
+        ilm77', 'film78', 'film79', 'film80', 'film81', 'film32', 'film83', 'film84', 'film85', 'film86', 'f\
+        ilm87', 'film88', 'film89', 'film90', 'film91', 'film32', 'film93', 'film94', 'film95', 'film96', 'f\
+        ilm97', 'film98', 'film99', 'film100']:
             if field in data:
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
@@ -250,6 +458,15 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Moovies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imdb = db.Column(db.String(140))
+    poster = db.Column(db.String(280))
+
+    def __repr__(self):
+        return '<Moovies {}>'.format(self.body)
 
 
 class Message(db.Model):
